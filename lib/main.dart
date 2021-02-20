5bb0a3ee0506a4928e6668fb66c7e7cecbbad7a4
FxMajid <akhfadlin@gmail.com>
2021-02-20 12:22:38 +0700

    new branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:state_management/BLoC/color_bloc.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ColorBloc bloc = ColorBloc();
-  @override
-  void dispose() {
-    bloc.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        floatingActionButton:
-            Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
-          FloatingActionButton(
-            onPressed: () {
-              bloc.evenSink.add(ColorEvent.to_amber);
-            },
-            backgroundColor: Colors.amber,
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              bloc.evenSink.add(ColorEvent.to_light_blue);
-            },
-            backgroundColor: Colors.lightBlue,
-          )
-        ]),
-        appBar: AppBar(
-          title: Text("BLoC Flutter"),
-        ),
-        body: Center(
-          child: StreamBuilder(
-            stream: bloc.stateStream,
-            initialData: Colors.amber,
-            builder: (context, snapshot) {
-              return AnimatedContainer(
-                duration: Duration(milliseconds: 500),
-                width: 300,
-                height: 300,
-                color: snapshot.data,
-              );
-            },
-          ),
-        ),
-      ),
+    return Container(
+      
     );
   }
 }
